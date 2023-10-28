3851343175016d65b4e0702c2bedeb826147f0e0
Jack Spence <jackspence123456@gmail.com>
2023-10-28 16:17:29 +0200

    Added & Exported Function to prompt users in CLI.

--- src/prompt.js
+++ src/prompt.js
@@ -1,5 +1,7 @@
+// Importing inquirer package for CLI prompting
 import inquirer from "inquirer";
 
+// Initial Questions
 const choiceMenu = [
 	{
 		name: "homeMenu",
@@ -16,3 +18,18 @@ const choiceMenu = [
 		],
 	},
 ];
+
+// Function to prompt users in CLI
+function userPrompt() {
+	inquirer
+		.prompt(choiceMenu)
+		.then((answers) => {
+			console.log(answers);
+		})
+		.catch((err) => console.error(err));
+}
+
+// Exporting the main function to prompt users in CLI
+export function prompt() {
+	userPrompt();
+}
