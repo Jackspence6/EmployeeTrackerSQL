c48841f8037388c1d1a796cfa2fc9585846bce9f
Jack Spence <jackspence123456@gmail.com>
2023-10-28 16:18:29 +0200

    Imported prompt function for main app function.

---
+++ index.js
@@ -0,0 +1,10 @@
+// Importing prompt function from src/prompt.js
+import { prompt } from "./src/prompt.js";
+
+// Main function for Application
+function main() {
+	prompt();
+}
+
+// Calling the Main application function to start the app
+main();
