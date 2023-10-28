3aad8526b473f26724d8b10e8708db30dd540449
Jack Spence <jackspence123456@gmail.com>
2023-10-28 15:51:39 +0200

    Added a choiceMenu.

--- src/prompt.js
+++ src/prompt.js
@@ -1,2 +1,18 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 
+const choiceMenu = [
+	{
+		name: "homeMenu",
+		type: "list",
+		message: "What would you like to do?",
+		choices: [
+			"View all Departments",
+			"View all Roles",
+			"View all Employees",
+			"Add a Department",
+			"Add a Role",
+			"Add an Employee",
+			"Update an Employee Role",
+		],
+	},
+];
