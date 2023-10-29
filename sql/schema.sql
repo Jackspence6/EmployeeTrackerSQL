5ebcb1900276a0fe25f04da851a9ba5f9ff4ca41
Jack Spence <jackspence123456@gmail.com>
2023-10-29 12:46:54 +0200

    Created employee db and add 3 tables to the db.

---
+++ sql/schema.sql
@@ -0,0 +1,31 @@
+/* Dropping employee database if one already exists  */
+DROP DATABASE IF EXISTS employee_db;
+
+/* Creating an employee Database */
+CREATE DATABASE employee_db;
+
+/* Using the employee database */
+USE employee_db;
+
+/* Creating a department table in the employee database  */
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+/* Creating a role table in the employee database  */
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL
+);
+
+/* Creating an employee table in the employee database  */
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
