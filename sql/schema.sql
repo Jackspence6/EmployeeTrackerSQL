22354fe23b029e63919b3c1ae4a61815a797022f
Jack Spence <jackspence123456@gmail.com>
2023-10-29 12:56:31 +0200

    Added AUTO_INCREMENT to table id's.

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,13 +9,13 @@ USE employee_db;
 
 /* Creating a department table in the employee database  */
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 /* Creating a role table in the employee database  */
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE role (
 
 /* Creating an employee table in the employee database  */
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
