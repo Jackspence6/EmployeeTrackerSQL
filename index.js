305ba3135056266de2c7214598005b7faea089b2
Jack Spence <jackspence123456@gmail.com>
2023-10-29 15:18:26 +0200

    Created function to display all department names & id's.

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 // Imports
-// const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const utilities = require("./src/utilities.js");
 const promptModule = require("./src/prompt.js");
-// const Connection = require("mysql2/typings/mysql/lib/Connection.js");
 
 // Global constants & variables
 const choiceMenu = promptModule.choiceMenu;
 const displayEmployeeManager = utilities.displayEmployeeManager;
-
-// // Open SQL Connection
-// function openDbConnection() {
-// 	console.log("Opening DataBase Connection...");
-
-// 	const connection = mysql.createConnection({
-// 		host: "localhost",
-// 		user: "root",
-// 		password: "Jackspence@6",
-// 		database: "employee_db",
-// 	});
-// 	return connection;
-// 	console.log("Connected to the employee_db DataBase!");
-// }
-
-// // Close SQL Connection
-// function closeDbConnection() {
-// 	Connection.end();
-// }
 
 // Main function for Application
 function main() {
---
+++ src/db.js
@@ -0,0 +1,30 @@
+// Imports
+const mysql = require("mysql2");
+
+// Open SQL Connection
+const db = mysql.createConnection(
+	{
+		host: "127.0.0.1",
+		user: "root",
+		password: "Jackspence@6",
+		database: "employee_db",
+	},
+	(err) => {
+		if (err) {
+			console.error("Error connecting to the database:", err);
+			return;
+		}
+		console.log("Connected to the employee_db DataBase!");
+	}
+);
+
+// Function to display all department names & id's
+function viewAllDepartments() {
+	db.query("SELECT * FROM department", (err, results) => {
+		if (err) {
+			console.error("Error fetching data:", err);
+			return;
+		}
+		console.table(results);
+	});
+}
