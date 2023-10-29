a7bbf739f15d37af131ed0a40f1ec1f328102e92
Jack Spence <jackspence123456@gmail.com>
2023-10-29 12:53:58 +0200

    Added PRIMARY KEY & FOREIGN KEY link for role & employee tables.

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE department (
 
 /* Creating a role table in the employee database  */
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT NOT NULL,
@@ -28,5 +28,6 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
